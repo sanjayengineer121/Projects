90671de5fa84d22da733a8f8bc6e9b6667da7e19
SanjayEngineer <33143882+sanjayengineer121@users.noreply.github.com>
2021-03-16 19:46:04 +0530

    Add files via upload

---
+++ madlibs/madlibs.py
@@ -0,0 +1,7 @@
+adj=input("Adjective :")
+verb=input("Verb :")
+famius_per==input("Person :")
+
+madlib=f"computer programming is so {adj}! it makes so exiceted all the time because \
+I love to {verb} stay hundred {verb} like you are a {famius_per}"
+print(madlib)
